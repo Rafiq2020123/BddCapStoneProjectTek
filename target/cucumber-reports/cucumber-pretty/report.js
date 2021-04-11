30947fec9d7047e9499835a2fd51a7dd96aadd43
Rafiq's code <ringo.cls2016@gmail.com>
2021-04-11 09:04:53 +0430

    commiting CapStone

---
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,485 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/ringo/eclipse-workspace/TekBDDProject/src/test/resources/Features/TekBddDesktop.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Desktop Features for project",
+  "description": "",
+  "id": "desktop-features-for-project",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 4286866300,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "User is on Retail website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "User Click on Desktops tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "User click on Show all desktops",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_is_on_Retail_website()"
+});
+formatter.result({
+  "duration": 4959420200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_Click_on_Desktops_tab()"
+});
+formatter.result({
+  "duration": 259223400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_on_Show_all_desktops()"
+});
+formatter.result({
+  "duration": 3801117300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "verify all items are present in Desktops tab",
+  "description": "",
+  "id": "desktop-features-for-project;verify-all-items-are-present-in-desktops-tab",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "User should see all items are present in Desktop page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_should_see_all_items_are_present_in_Desktop_page()"
+});
+formatter.result({
+  "duration": 1664662400,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1064802100,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1865684800,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "User is on Retail website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "User Click on Desktops tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "User click on Show all desktops",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_is_on_Retail_website()"
+});
+formatter.result({
+  "duration": 5350477800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_Click_on_Desktops_tab()"
+});
+formatter.result({
+  "duration": 332874400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_on_Show_all_desktops()"
+});
+formatter.result({
+  "duration": 3790720600,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "User add HP LP 3065 from Desktops tab to the cart",
+  "description": "",
+  "id": "desktop-features-for-project;user-add-hp-lp-3065-from-desktops-tab-to-the-cart",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "User click ADD TO CART option on \u0027HP LP3065\u0027 item",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User select quantity 1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "User click add to Cart button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "User should see a message \u0027Success: you have added HP LP 3065 to your Shopping cart!\u0027",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "3065",
+      "offset": 39
+    }
+  ],
+  "location": "BddDesktopStepDefinetion.user_click_ADD_TO_CART_option_on_HP_LP_item(int)"
+});
+formatter.result({
+  "duration": 124747400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 21
+    }
+  ],
+  "location": "BddDesktopStepDefinetion.user_select_quantity(int)"
+});
+formatter.result({
+  "duration": 3017800500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_add_to_Cart_button()"
+});
+formatter.result({
+  "duration": 544238800,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "3065",
+      "offset": 57
+    }
+  ],
+  "location": "BddDesktopStepDefinetion.user_should_see_a_message_Success_you_have_added_HP_LP_to_your_Shopping_cart(int)"
+});
+formatter.result({
+  "duration": 706010700,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 811959200,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1816792600,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "User is on Retail website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "User Click on Desktops tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "User click on Show all desktops",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_is_on_Retail_website()"
+});
+formatter.result({
+  "duration": 4453051300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_Click_on_Desktops_tab()"
+});
+formatter.result({
+  "duration": 186644800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_on_Show_all_desktops()"
+});
+formatter.result({
+  "duration": 4736523000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "User add Canon EOS 5D from Desktops tab to the cart",
+  "description": "",
+  "id": "desktop-features-for-project;user-add-canon-eos-5d-from-desktops-tab-to-the-cart",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 18,
+  "name": "User click ADD TO CART option on \u0027Canon EOS 5D\u0027 item",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "User select color from dropdown \u0027Red\u0027",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "User select quuantity 1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "User click add too Cart button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "User should see a message \u0027Succes: You have added Canon EOS 5d to your shopping cart\u0027",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "5",
+      "offset": 44
+    }
+  ],
+  "location": "BddDesktopStepDefinetion.user_click_ADD_TO_CART_option_on_Canon_EOS_D_item(int)"
+});
+formatter.result({
+  "duration": 94929600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_select_color_from_dropdown_Red()"
+});
+formatter.result({
+  "duration": 2753772200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 22
+    }
+  ],
+  "location": "BddDesktopStepDefinetion.user_select_quuantity(int)"
+});
+formatter.result({
+  "duration": 1230599000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_add_too_Cart_button()"
+});
+formatter.result({
+  "duration": 617477000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "5",
+      "offset": 60
+    }
+  ],
+  "location": "BddDesktopStepDefinetion.user_should_see_a_message_Succes_You_have_added_Canon_EOS_d_to_your_shopping_cart(int)"
+});
+formatter.result({
+  "duration": 1583058100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 4743649400,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1736313200,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "User is on Retail website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "User Click on Desktops tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "User click on Show all desktops",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_is_on_Retail_website()"
+});
+formatter.result({
+  "duration": 4511634300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_Click_on_Desktops_tab()"
+});
+formatter.result({
+  "duration": 204946400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_on_Show_all_desktops()"
+});
+formatter.result({
+  "duration": 4676690000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 25,
+  "name": "User add a review to Canon EOS 5D item in Desktops tab",
+  "description": "",
+  "id": "desktop-features-for-project;user-add-a-review-to-canon-eos-5d-item-in-desktops-tab",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 26,
+  "name": "User click on Canon EOS 5D item",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "User click on write a review link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "user fill the review information with below information",
+  "rows": [
+    {
+      "cells": [
+        "yourname",
+        "yourReview",
+        "Rating"
+      ],
+      "line": 29
+    },
+    {
+      "cells": [
+        "Arad",
+        "It was an Average review for me",
+        "Bad"
+      ],
+      "line": 30
+    }
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "User click on Continue Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "User should see a message with \u0027Thank you for your review. It has been submitted to the webmaster for approval\u0027",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "5",
+      "offset": 24
+    }
+  ],
+  "location": "BddDesktopStepDefinetion.user_click_on_Canon_EOS_D_item(int)"
+});
+formatter.result({
+  "duration": 1711256700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_on_write_a_review_link()"
+});
+formatter.result({
+  "duration": 1194079600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_fill_the_review_information_with_below_information(DataTable)"
+});
+formatter.result({
+  "duration": 1898130500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_click_on_Continue_Button()"
+});
+formatter.result({
+  "duration": 1635192300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BddDesktopStepDefinetion.user_should_see_a_message_with_Thank_you_for_your_review_It_has_been_submitted_to_the_webmaster_for_approval()"
+});
+formatter.result({
+  "duration": 39757400,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 819493500,
+  "status": "passed"
+});
+});
